aaa29ae27e5d4f0a7d55bdb128e53b7b8a727a01
Lucas Waunn <lucaswaunn@gmail.com>
2026-09-16 11:35:54 -0500

    Adding Account button to top left, and pill in top right. First commit!

--- New-Things-Tracker/ViewController.swift
+++ New-Things-Tracker/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Background color
+        view.backgroundColor = UIColor(named: "FogBackground")
+
+        // Profile button (top left)
+        let profileButton = UIBarButtonItem(
+            image: UIImage(systemName: "person.circle.fill"),
+            style: .plain,
+            target: self,
+            action: #selector(profileTapped)
+        )
+        navigationItem.leftBarButtonItem = profileButton
+
+        // Counter pill (top right) - we'll make this a custom view
+        let counterLabel = UILabel()
+        counterLabel.text = "0 firsts"
+        counterLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
+        counterLabel.textColor = UIColor(named: "FogBackground")
+        counterLabel.backgroundColor = UIColor(named: "DeepPineInk")
+        counterLabel.textAlignment = .center
+        counterLabel.layer.cornerRadius = 12
+        counterLabel.clipsToBounds = true
+        counterLabel.frame = CGRect(x: 0, y: 0, width: 80, height: 28)
+
+        let counterItem = UIBarButtonItem(customView: counterLabel)
+        navigationItem.rightBarButtonItem = counterItem
+    }
+    
+    @objc func profileTapped() {
+        print("Profile tapped")
     }
 
 
