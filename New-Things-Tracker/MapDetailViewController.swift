f7b4b7be6b975fc02a751d00699ded37cd2eb7f9
Lucas Waunn <lucaswaunn@gmail.com>
2026-09-16 14:33:32 -0500

    Creating Map page and dummy data for First times, account page in top right, and nav bar at the bottom

---
+++ New-Things-Tracker/MapDetailViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import MapKit
+
+class MapDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Map"
+
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mapView)
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+}
--- New-Things-Tracker/ViewController.swift
+++ New-Things-Tracker/ViewController.swift
@@ -10,39 +10,225 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    private var profileButtonView: UIButton!
+    private var mapView: MKMapView!
+    private var islandBar: UIView!
+    private var tableView: UITableView!
+    private var homeButton: UIButton!
+    private var mapButton: UIButton!
+    private var isMapVisible = false
+
+    private let sections: [(month: String, firsts: [First])] = [
+        ("SEPTEMBER", [
+            First(title: "Randolph Street Market", location: "West Loop", category: "Festival",
+                  date: "Sat 6 Sep", duration: "4h 12m", photoCount: 23, extraPhotos: 21,
+                  largePhotoColor: UIColor(red: 0.88, green: 0.78, blue: 0.72, alpha: 1),
+                  smallPhotoColor: UIColor(red: 0.82, green: 0.70, blue: 0.63, alpha: 1)),
+            First(title: "First Jazz Concert", location: "River North", category: "Music",
+                  date: "Fri 19 Sep", duration: "2h 45m", photoCount: 11, extraPhotos: 9,
+                  largePhotoColor: UIColor(named: "DustySage") ?? .systemGray,
+                  smallPhotoColor: UIColor(named: "DustySage")?.withAlphaComponent(0.7) ?? .systemGray2),
+        ]),
+        ("AUGUST", [
+            First(title: "Hot Air Balloon Ride", location: "Napa Valley", category: "Adventure",
+                  date: "Sun 10 Aug", duration: "1h 30m", photoCount: 34, extraPhotos: 31,
+                  largePhotoColor: UIColor(red: 0.79, green: 0.87, blue: 0.82, alpha: 1),
+                  smallPhotoColor: UIColor(red: 0.70, green: 0.80, blue: 0.74, alpha: 1)),
+            First(title: "Drive-In Movie Night", location: "Wicker Park", category: "Entertainment",
+                  date: "Sat 23 Aug", duration: "3h 05m", photoCount: 8, extraPhotos: 6,
+                  largePhotoColor: UIColor(named: "ClayAccent")?.withAlphaComponent(0.4) ?? .systemOrange,
+                  smallPhotoColor: UIColor(named: "ClayAccent")?.withAlphaComponent(0.25) ?? .systemOrange),
+        ]),
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Background color
-        view.backgroundColor = UIColor(named: "FogBackground")
+        view.backgroundColor = UIColor(named: "DustySage")
+        navigationController?.navigationBar.isHidden = true
 
-        // Profile button (top left)
-        let profileButton = UIBarButtonItem(
-            image: UIImage(systemName: "person.circle.fill"),
-            style: .plain,
-            target: self,
-            action: #selector(profileTapped)
-        )
-        navigationItem.leftBarButtonItem = profileButton
-
-        // Counter pill (top right) - we'll make this a custom view
-        let counterLabel = UILabel()
-        counterLabel.text = "0 firsts"
-        counterLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
-        counterLabel.textColor = UIColor(named: "FogBackground")
-        counterLabel.backgroundColor = UIColor(named: "DeepPineInk")
-        counterLabel.textAlignment = .center
-        counterLabel.layer.cornerRadius = 12
-        counterLabel.clipsToBounds = true
-        counterLabel.frame = CGRect(x: 0, y: 0, width: 80, height: 28)
-
-        let counterItem = UIBarButtonItem(customView: counterLabel)
-        navigationItem.rightBarButtonItem = counterItem
+        setupMapView()
+        setupProfileButton()
+        setupIslandBar()
+        setupCardsTable()
     }
-    
+
+    private func setupMapView() {
+        mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.alpha = 0
+        view.addSubview(mapView)
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+
+    private func setupProfileButton() {
+        profileButtonView = UIButton(type: .system)
+        profileButtonView.translatesAutoresizingMaskIntoConstraints = false
+        profileButtonView.setImage(UIImage(systemName: "person.fill"), for: .normal)
+        profileButtonView.tintColor = UIColor(named: "DeepPineInk")
+        profileButtonView.backgroundColor = UIColor(named: "FogBackground")
+        profileButtonView.layer.cornerRadius = 17
+        profileButtonView.layer.shadowColor = UIColor.black.cgColor
+        profileButtonView.layer.shadowOpacity = 0.15
+        profileButtonView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        profileButtonView.layer.shadowRadius = 6
+        profileButtonView.addTarget(self, action: #selector(profileTapped), for: .touchUpInside)
+        view.addSubview(profileButtonView)
+
+        NSLayoutConstraint.activate([
+            profileButtonView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            profileButtonView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            profileButtonView.widthAnchor.constraint(equalToConstant: 34),
+            profileButtonView.heightAnchor.constraint(equalToConstant: 34),
+        ])
+    }
+
+    private func setupCardsTable() {
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 110, right: 0)
+        tableView.register(FirstCardCell.self, forCellReuseIdentifier: FirstCardCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.insertSubview(tableView, belowSubview: islandBar)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: profileButtonView.bottomAnchor, constant: 8),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    private func setupIslandBar() {
+        islandBar = UIView()
+        islandBar.translatesAutoresizingMaskIntoConstraints = false
+        islandBar.backgroundColor = UIColor(named: "DeepPineInk")
+        islandBar.layer.cornerRadius = 30
+        islandBar.layer.shadowColor = UIColor.black.cgColor
+        islandBar.layer.shadowOpacity = 0.2
+        islandBar.layer.shadowOffset = CGSize(width: 0, height: 4)
+        islandBar.layer.shadowRadius = 12
+        view.addSubview(islandBar)
+
+        NSLayoutConstraint.activate([
+            islandBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            islandBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            islandBar.heightAnchor.constraint(equalToConstant: 60),
+            islandBar.widthAnchor.constraint(equalToConstant: 160),
+        ])
+
+        homeButton = makeIslandButton(image: UIImage(named: "icon-house"), action: #selector(homeTapped))
+        mapButton  = makeIslandButton(image: UIImage(named: "icon-map"),   action: #selector(mapTapped))
+
+        islandBar.addSubview(homeButton)
+        islandBar.addSubview(mapButton)
+
+        updateIslandSelection()
+
+        NSLayoutConstraint.activate([
+            homeButton.leadingAnchor.constraint(equalTo: islandBar.leadingAnchor, constant: 28),
+            homeButton.centerYAnchor.constraint(equalTo: islandBar.centerYAnchor),
+
+            mapButton.trailingAnchor.constraint(equalTo: islandBar.trailingAnchor, constant: -28),
+            mapButton.centerYAnchor.constraint(equalTo: islandBar.centerYAnchor),
+        ])
+    }
+
+    private func makeIslandButton(image: UIImage?, action: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(image, for: .normal)
+        button.tintColor = UIColor(named: "FogBackground")
+        button.addTarget(self, action: action, for: .touchUpInside)
+        button.addTarget(self, action: #selector(islandButtonPressDown(_:)), for: .touchDown)
+        button.addTarget(self, action: #selector(islandButtonPressUp(_:)),   for: [.touchUpInside, .touchUpOutside, .touchCancel])
+        return button
+    }
+
+    @objc private func islandButtonPressDown(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseIn, .allowUserInteraction]) {
+            sender.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+        }
+    }
+
+    @objc private func islandButtonPressUp(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.8, options: .allowUserInteraction) {
+            sender.transform = .identity
+        }
+    }
+
     @objc func profileTapped() {
         print("Profile tapped")
     }
 
+    private func updateIslandSelection() {
+        homeButton.setImage(UIImage(named: isMapVisible ? "icon-house"        : "icon-house-filled"), for: .normal)
+        mapButton.setImage(UIImage(named:  isMapVisible ? "icon-map-filled"   : "icon-map"),          for: .normal)
+    }
 
+    @objc func homeTapped() {
+        guard isMapVisible else { return }
+        isMapVisible = false
+        mapView.alpha = 0
+        tableView.isHidden = false
+        profileButtonView.isHidden = false
+        updateIslandSelection()
+    }
+
+    @objc func mapTapped() {
+        guard !isMapVisible else { return }
+        isMapVisible = true
+        mapView.alpha = 1
+        tableView.isHidden = true
+        profileButtonView.isHidden = true
+        updateIslandSelection()
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sections[section].firsts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FirstCardCell.identifier, for: indexPath) as! FirstCardCell
+        cell.configure(with: sections[indexPath.section].firsts[indexPath.row])
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let container = UIView()
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let attrs: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 12, weight: .semibold),
+            .foregroundColor: UIColor(named: "FogBackground")?.withAlphaComponent(0.7) ?? UIColor.white,
+            .kern: 1.5,
+        ]
+        label.attributedText = NSAttributedString(string: sections[section].month, attributes: attrs)
+        container.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 20),
+            label.topAnchor.constraint(equalTo: container.topAnchor, constant: 8),
+            label.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -4),
+        ])
+        container.backgroundColor = .clear
+        return container
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        36
+    }
+}
