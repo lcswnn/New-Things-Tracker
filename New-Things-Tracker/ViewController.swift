83f051714488538c62d3c21e634f19c9f792d1b2
Lucas Waunn <lucaswaunn@gmail.com>
2026-09-16 11:07:45 -0500

    Initial Commit

---
+++ New-Things-Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New-Things-Tracker
+//
+//  Created by Lucas Waunn on 9/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ New-Things-TrackerTests/New_Things_TrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  New_Things_TrackerTests.swift
+//  New-Things-TrackerTests
+//
+//  Created by Lucas Waunn on 9/16/26.
+//
+
+import Testing
+@testable import New_Things_Tracker
+
+struct New_Things_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
